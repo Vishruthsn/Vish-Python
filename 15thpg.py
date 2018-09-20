44198b74eb7b130443963127caf4c506d4e0fdda
Vishruthsn <43022905+Vishruthsn@users.noreply.github.com>
2018-09-20 10:33:35 +0530

    Add files via upload

---
+++ 15thpg.py
@@ -0,0 +1,3 @@
+for a in range(1,21):
+	for i in range(1,11):
+		print(a,"*",i,"=",a*i)
---
+++ 16thpg.py
@@ -0,0 +1,4 @@
+n=int(input("Enter a number :"))
+for a in range(1,n+1):
+	for a in range (1,11):
+		print(a,"*",i,"=",a*i)
